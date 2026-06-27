dda13d605d06a16f079eb9a741eab1ca9e33588b
Abhishek Rathour <abhishekrathour826660@gmail.com>
2026-06-27 18:15:24 +0530

    Update app.py

--- HOUSE PRICE PREDICTION/app.py
+++ HOUSE PRICE PREDICTION/app.py
@@ -1,53 +1,8 @@
-# import streamlit as st
-# import joblib
-# import numpy as np
-
-# model = joblib.load("house_price_model.pkl")
-
-# st.title("🏠 House Price Prediction")
-
-# st.write("Enter house details")
-
-# lot_area = st.number_input("Lot Area")
-# overall_qual = st.number_input("Overall Quality")
-# year_built = st.number_input("Year Built")
-# gr_liv_area = st.number_input("Ground Living Area")
-
-
-# if st.button("Predict Price"):
-
-#     input_data = np.array(
-#         [[
-#             lot_area,
-#             overall_qual,
-#             year_built,
-#             gr_liv_area
-#         ]]
-#     )
-
-#     prediction = model.predict(input_data)
-
-#     st.success(
-#         f"Estimated House Price: ${prediction[0]:,.2f}"
-#     )
-
-
-
-
-
-
-
 import streamlit as st
 import joblib
 import numpy as np
-import os
 
-model_path = os.path.join(
-    os.path.dirname(__file__),
-    "house_price_model.pkl"
-)
-
-model = joblib.load(model_path)
+model = joblib.load("house_price_model.pkl")
 
 st.title("🏠 House Price Prediction")
 
@@ -75,4 +30,5 @@ if st.button("Predict Price"):
     st.success(
         f"Estimated House Price: ${prediction[0]:,.2f}"
     )
+
 
